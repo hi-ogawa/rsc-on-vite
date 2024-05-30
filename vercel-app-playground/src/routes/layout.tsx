641fd549ac73bf4d49d0c5518f529c1459f584a5
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-30 18:21:03 +0900

    chore: copy starter template

---
+++ vercel-app-playground/src/routes/layout.tsx
@@ -0,0 +1,36 @@
+import { Link } from '@hiogawa/react-server/client';
+
+export default function Layout(props: React.PropsWithChildren) {
+  return (
+    <html>
+      <head>
+        <meta charSet="UTF-8" />
+        <title>React Server Starter</title>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body>
+        <h3>React Server Starter</h3>
+        <a
+          href="https://github.com/hi-ogawa/vite-plugins/tree/main/packages/react-server"
+          target="_blank"
+        >
+          GitHub
+        </a>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/use-state">Counter (useState)</Link>
+            </li>
+            <li>
+              <Link href="/server-action">Counter (server action)</Link>
+            </li>
+          </ul>
+        </nav>
+        {props.children}
+      </body>
+    </html>
+  );
+}
---
+++ vercel-app-playground/src/routes/server-action/page.tsx
@@ -0,0 +1,10 @@
+import { getCounter } from './_action';
+import { Counter } from './_client';
+
+export default async function Page() {
+  return (
+    <div>
+      <Counter value={getCounter()} />
+    </div>
+  );
+}
