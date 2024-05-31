4f87c2ff85c691f2d0a064f2f60ec7a7214451d9
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-05-31 18:33:03 +0900

    chore(vercel-app-playground): port `/patterns/search-params`

---
+++ vercel-app-playground/src/routes/patterns/page.tsx
@@ -0,0 +1,62 @@
+import { ExternalLink } from '#/ui/external-link';
+import { Link } from '@hiogawa/react-server/client';
+import clsx from 'clsx';
+
+const items = [
+  {
+    name: 'Active links',
+    slug: 'active-links',
+    description: 'Update the style of the current active link',
+  },
+  {
+    name: 'Breadcrumbs',
+    slug: 'breadcrumbs',
+    description: 'Shared server-side Breadcrumb UI using Parallel Routes',
+  },
+  {
+    name: 'Updating URL search params',
+    slug: 'search-params',
+    description: 'Update searchParams using `useRouter` and `<Link>`',
+    ok: true,
+  },
+];
+
+export default function Page() {
+  return (
+    <div className="space-y-6">
+      <title>Patterns</title>
+      <h1 className="text-xl font-medium text-gray-300">Patterns</h1>
+
+      <div className="grid grid-cols-1 gap-5 lg:grid-cols-2">
+        {items.map((item) => {
+          return (
+            <Link
+              href={`/patterns/${item.slug}`}
+              key={item.name}
+              className={clsx(
+                'group block space-y-1.5 rounded-lg bg-gray-900 px-5 py-3 hover:bg-gray-800',
+                !item.ok && 'line-through opacity-70',
+              )}
+            >
+              <div className="font-medium text-gray-200 group-hover:text-gray-50">
+                {item.name}
+              </div>
+
+              {item.description ? (
+                <div className="line-clamp-3 text-sm text-gray-400 group-hover:text-gray-300">
+                  {item.description}
+                </div>
+              ) : null}
+            </Link>
+          );
+        })}
+      </div>
+
+      <div className="flex gap-2">
+        <ExternalLink href="https://github.com/vercel/app-playground/tree/main/app/patterns">
+          Code
+        </ExternalLink>
+      </div>
+    </div>
+  );
+}
---
+++ vercel-app-playground/src/routes/patterns/search-params/active-link.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { Link, useRouter } from '@hiogawa/react-server/client';
+import clsx from 'clsx';
+
+export default function ActiveLink({
+  isActive,
+  searchParams,
+  children,
+}: {
+  isActive: boolean;
+  searchParams: string;
+  children: React.ReactNode;
+}) {
+  const pathname = useRouter((s) => s.location.pathname);
+
+  return (
+    <Link
+      className={clsx('rounded-lg px-3 py-1 text-sm font-medium no-underline', {
+        'bg-gray-700 text-gray-100 hover:bg-gray-500 hover:text-white':
+          !isActive,
+        'bg-vercel-blue text-white': isActive,
+      })}
+      href={pathname + '?' + searchParams}
+    >
+      {children}
+    </Link>
+  );
+}
