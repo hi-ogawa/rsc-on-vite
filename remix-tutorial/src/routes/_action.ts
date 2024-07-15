0cc2969ad9185928ca062aa5c6fa00828543be2b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-15 09:33:31 +0900

    chore(deps): update v0.3.3 (#15)

--- remix-tutorial/src/routes/_action.ts
+++ remix-tutorial/src/routes/_action.ts
@@ -9,11 +9,5 @@ export async function actionDeleteContact(id: string) {
   const contact = await fakeContacts.get(id);
   tinyassert(contact);
   fakeContacts.destroy(contact.id);
-
-  // TODO
-  // action response stream renders `src/routes/contacts/[contactId]/page.tsx`
-  // but contact doesn't exists anymore and server component throws,
-  // which makes this redirect error to not caught by client
-  // when users don't have custom error page
   throw redirect("/");
 }
